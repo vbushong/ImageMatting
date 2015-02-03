97c6da86cbfdfb5804d76624604df506de35086c
Vanna Bushong <vbushong@ucla.edu>
2015-02-03 08:54:44 -0500

    Affinity matrix

--- ImageMatting/ImageMatting.cpp
+++ ImageMatting/ImageMatting.cpp
@@ -5,16 +5,38 @@
 using namespace std;
 using namespace cv;
 
-Mat image;
+Mat source;
+Mat grayscale;
+Mat affinity;
 
 void main() 
 {
 	// Load image
-	image = imread("cubs.jpg");
+	source = imread("square.png", CV_LOAD_IMAGE_GRAYSCALE);
+	int i = source.type();
+
+	int numPix = source.rows * source.cols;
+
+	// Create affinity matrix
+	affinity = Mat::zeros(numPix, numPix, CV_8UC1);
+
+	int diff = 0;
+	for (int i=0; i<numPix; i++)
+	{
+		for (int j=0; j<numPix; j++)
+		{
+			diff = j - i;
+			affinity.at<uchar>(i,j) = diff;
+			affinity.at<uchar>(j,i) = diff;
+		}
+	}
 
 	// Display image
 	namedWindow("Image Matting");
-	imshow("Image Matting", image);
+	imshow("Image Matting", source);
+
+	namedWindow("Affinity Matrix");
+	imshow("Affinity Matrix", affinity);
 
 	// Hold window until escape key pressed
 	waitKey(0);
